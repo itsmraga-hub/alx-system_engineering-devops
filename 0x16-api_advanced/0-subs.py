586a32b9c182fbdf927c226bd461f9709d927ea3
itsmraga-hub <ragawilliam570@gmail.com>
2023-06-07 17:40:02 +0300

    Task 0: correction

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
-"""a function that queries the Reddit API returning number of subs"""
-
+""" Function that queries the Reddit API """
 import requests
+import sys
 
 
 def number_of_subscribers(subreddit):
-    """
-        number of subscribers function definition
-    """
-    url = 'https://www.reddit.com/r/'
-    if subreddit:
-        headers = {'User-Agent': 'itsmraga'}
-        r = requests.get(f'{url}{subreddit}/about.json', headers=headers,
-                         allow_redirects=False)
-        res = r.json()
-        if r.status_code == 200:
-            subscribers = res.get("data", {}).get("subscribers", 0)
-            return subscribers
+    """  Args:
+        subreddit: subreddit name
+    Returns:
+        number of subscribers to the subreddit,
+        or 0 if subreddit requested is invalid"""
+    headers = {'User-Agent': 'itsmraga'}
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
+    if response.status_code == 200:
+        return (response.json().get("data").get("subscribers"))
     return (0)
