64d7c5d3b916a5ab6d797eda1eb720bcf3411f51
itsmraga-hub <ragawilliam570@gmail.com>
2023-06-07 17:43:15 +0300

    Update task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,12 +8,16 @@ def number_of_subscribers(subreddit):
     """  Args:
         subreddit: subreddit name
     Returns:
-        number of subscribers to the subreddit,
-        or 0 if subreddit requested is invalid"""
+        no of subscribers or 0 if subreddit requested is invalid"""
+
     headers = {'User-Agent': 'itsmraga'}
+
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
-        return (response.json().get("data").get("subscribers"))
+        subscribers = response.json().get("data").get("subscribers")
+        return (subscribers)
+
     return (0)
