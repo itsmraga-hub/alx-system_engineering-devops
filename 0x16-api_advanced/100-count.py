5b6d0fd576a1396b700f1fdba5a078370df6cbb9
itsmraga-hub <ragawilliam570@gmail.com>
2023-06-07 18:03:49 +0300

    100-count solution

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-"""
-    Write a recursive function that queries the Reddit API, parses
-    the title of all hot articles, and prints a sorted count of
-    given keywords (case-insensitive, delimited by spaces.
-    Javascript should count as javascript, but java should not).
-"""
+""" Write a recursive function that queries the Reddit API, parses """
 
 import requests
+import sys
 
 
 def count_words(subreddit, word_list, after='', word_dict={}):
