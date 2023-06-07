3fde4913c20416c45fc0164f0a3f8c55cbfa3c72
itsmraga-hub <ragawilliam570@gmail.com>
2023-06-07 18:00:42 +0300

    Task 3: recursively query reddit API

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
-"""
-    Write a recursive function that queries the Reddit API and returns
-    a list containing the titles of all hot articles for a given subreddit.
-    If no results are found for the given subreddit,
-    the function should return None.
-"""
+""" A recursive function that queries the Reddit API """
 
 import requests
+import sys
+
 
 def recurse(subreddit, hot_list=[], after=''):
-    """
-        recursive function that queries Reddit API
+    """Returns:
+          list containing the titles of all hot articles for the subreddit
+          or None if queried subreddit is invalid
     """
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    
+
     headers = {'User-Agent': 'itsmraga'}
     r = requests.get(url, headers=headers, params={'after': after},
-            allow_redirects=False)
+                     allow_redirects=False)
 
     if r.status_code == 200:
         next_ = r.json().get('data').get('after')
